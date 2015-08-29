0e4af2821985e7008ee1c953d926297c575dbccf
Leon Denise <lowpolygonum@gmail.com>
2015-08-29 18:12:52 +0200

    fix require issue

--- LD33PostCompo/scripts/core/logic.js
+++ LD33PostCompo/scripts/core/logic.js
@@ -1,6 +1,6 @@
 
 define(['../settings', '../core/manager', '../core/renderer',
-'../element/Phylactere',
+'../element/phylactere',
 '../base/utils', '../base/point', '../base/color'], function(Settings, Manager, renderer, Phylactere, Utils, Point, Color)
 {
   var Logic = {}
