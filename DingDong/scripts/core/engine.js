7e43054f87d4e79c70b8edee01dfedea7473bff6
Leon <leon.denise@free.fr>
2015-09-22 23:37:41 +0200

    DingDong

---
+++ DingDong/scripts/core/engine.js
@@ -0,0 +1,84 @@
+
+define(['../lib/pixi', '../core/render',
+'../control/mouse', '../control/keyboard',
+'../base/utils', '../base/global', '../base/animation'],
+function(PIXI, Render, Mouse, Keyboard, Utils, Global, Animation)
+{
+  var Engine = {}
+
+  Engine.init = function ()
+  {
+    // Events
+    Render.layerDraw.interactive = true
+    Render.layerDraw.on('mousedown', Mouse.onClic).on('touchstart', Mouse.onClic)
+    Render.layerDraw.on('mouseup', Mouse.onMouseUp)
+    Render.layerDraw.on('mousemove', Mouse.onMove).on('touchmove', Mouse.onMove)
+    document.addEventListener('keydown', Keyboard.onKeyDown)
+    document.addEventListener('keyup', Keyboard.onKeyUp)
+
+    Global.timeStarted = new Date() / 1000
+    Global.timeElapsed = 0
+
+    Engine.impulse = new Animation.add(false, 1, function(ratio){
+      ratio = 1 - Math.sin(ratio*Utils.PI2)
+      Render.getFilter().bufferTreshold = ratio * 0.5 + 0.25
+    })
+	}
+
+  Engine.update = function ()
+  {
+    Global.timeElapsed = new Date() / 1000 - Global.timeStarted
+
+    if (Keyboard.P.down)
+    {
+      Global.pause = !Global.pause
+      Keyboard.P.down = false
+      if (Global.pause)
+      {
+        Render.video.pause()
+      }
+      else
+      {
+        Render.video.play()
+      }
+    }
+
+    if (Keyboard.Left.down)
+    {
+      Render.previousFilter()
+      Keyboard.Left.down = false
+    }
+
+    if (Keyboard.Right.down)
+    {
+      Render.nextFilter()
+      Keyboard.Right.down = false
+    }
+
+    if (Keyboard.Space.down)
+    {
+      Engine.impulse.start()
+      Keyboard.Space.down = false
+    }
+
+    if (Keyboard.S.down)
+    {
+      Render.video.shuffle()
+      Keyboard.S.down = false
+    }
+
+    //Engine.layerDraw.scale.x = Engine.layerDraw.scale.y = 1 + Engine.mouse.x * 8 / Engine.getWidth()
+    // Engine.layerDraw.x = offsetPan.x + (Engine.layerDraw.scale.x - 1) * -Engine.getWidth() / 2
+    // Engine.layerDraw.y = offsetPan.y + (Engine.layerDraw.scale.y - 1) * -Engine.getHeight() / 2
+
+    //Render.getFilter().pixelSize = 1.0 + Math.ceil(Engine.mouse.x * 8 / Engine.getWidth())
+
+    Render.getFilter().mouseX = Mouse.x / Engine.getWidth();
+    Render.getFilter().mouseY = Mouse.y / Engine.getHeight();
+  }
+
+  Engine.getWidth = function () { return window.innerWidth }
+  Engine.getHeight = function () { return window.innerHeight }
+
+  return Engine
+})
---
+++ DingDong/scripts/core/filter.js
@@ -0,0 +1,95 @@
+
+define(['../lib/pixi', '../base/global'], function (PIXI, Global)
+{
+  function Filter(fragmentSrc)
+  {
+      PIXI.AbstractFilter.call(this,
+          // vertex shader
+          null,
+          // fragment shader
+          fragmentSrc,
+          // custom uniforms
+          {
+              uResolution: { type: '2fv', value: new Float32Array([0, 0]) }
+              ,uTime:  { type: '1f', value: 0 }
+              ,uMouse:  { type: '2fv', value: new Float32Array([0, 0]) }
+              ,uPixelSize:  { type: '1f', value: 1 }
+              ,uFrequenceTotal:  { type: '1f', value: 0 }
+              ,uBuffer: { type: 'sampler2D', value: 0 }
+              ,uFrequenceTexture: { type: 'sampler2D', value: 0 }
+              ,uVideo: { type: 'sampler2D', value: 0 }
+              ,uBufferTreshold: { type: '1f', value: Global.bufferTreshold }
+              ,uFilter5x5Gaussian: { type: '1fv', value: new Float32Array([
+          			-1,-1,-1,-1,-1,
+          			-1,-1,-1,-1,-1,
+          			-1,-1,24,-1,-1,
+          			-1,-1,-1,-1,-1,
+          			-1,-1,-1,-1,-1]) }
+              ,uFilter5x5Neighbor: { type: '1fv', value: new Float32Array([
+                0.04,0.04,0.04,0.04,0.04,
+          			0.04,0.04,0.04,0.04,0.04,
+          			0.04,0.04,0.04,0.04,0.04,
+          			0.04,0.04,0.04,0.04,0.04,
+          			0.04,0.04,0.04,0.04,0.04]) }
+              ,uFilter3x3Neighbor: { type: '1fv', value: new Float32Array([
+                1/9,1/9,1/9,
+                1/9,1/9,1/9,
+                1/9,1/9,1/9]) }
+          }
+      )
+  }
+
+  Filter.prototype = Object.create(PIXI.AbstractFilter.prototype)
+  Filter.prototype.constructor = Filter
+
+  Object.defineProperties(Filter.prototype, {
+    resolution  :{set:function(value){
+      this.uniforms.uResolution.value  =value}}
+  })
+  Object.defineProperties(Filter.prototype, {
+    time  :{set:function(value){
+      this.uniforms.uTime.value  =value}}
+  })
+  Object.defineProperties(Filter.prototype, {
+    mouse  :{set:function(value){
+      this.uniforms.uMouse.value[0]  = value.x
+      this.uniforms.uMouse.value[1]  = value.y
+    }}
+  })
+  Object.defineProperties(Filter.prototype, {
+    mouseX  :{set:function(value){
+      this.uniforms.uMouse.value[0]  = value
+    }}
+  })
+  Object.defineProperties(Filter.prototype, {
+    mouseY  :{set:function(value){
+      this.uniforms.uMouse.value[1]  = value
+    }}
+  })
+  Object.defineProperties(Filter.prototype, {
+    frequenceTotal  :{set:function(value){
+      this.uniforms.uFrequenceTotal.value  =value}}
+  })
+  Object.defineProperties(Filter.prototype, {
+    pixelSize  :{set:function(value){
+      this.uniforms.uPixelSize.value  =value}}
+  })
+  Object.defineProperties(Filter.prototype, {
+    buffer  :{set:function(value){
+      this.uniforms.uBuffer.value  =value}}
+  })
+  Object.defineProperties(Filter.prototype, {
+    video  :{set:function(value){
+      this.uniforms.uVideo.value  =value}}
+  })
+  Object.defineProperties(Filter.prototype, {
+    frequenceTexture  :{set:function(value){
+      this.uniforms.uFrequenceTexture.value  =value}}
+  })
+  Object.defineProperties(Filter.prototype, {
+    bufferTreshold  :{set:function(value){
+      this.uniforms.uBufferTreshold.value  =value}}
+  })
+
+  return Filter
+})
