0a03e03fa7fafc658f466eed89f6f439490de5dd
Leon <lowpolygonum@gmail.com>
2026-01-29 11:35:33 +0100

    fix crash party pattern generator cache

--- w/crash-party-pattern/code/script/files.js
+++ w/crash-party-pattern/code/script/files.js
@@ -8,12 +8,12 @@ var files = [
 ]
 
 // timestamp to ignore cache when reload
-// const ts = Date.now().toString()
+const ts = Date.now().toString()
 
 // load file
 const path = "./code/shader/"
 for (const file in files) {
-    await fetch(path+files[file])//+"?ts="+ts)
+    await fetch(path+files[file]+"?ts="+ts)
     .then(t => t.text())
     .then(t => shaders[files[file]] = t)
 }
