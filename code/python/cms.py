fa004bcc95c0c51ac4ac60f9cfe22fe6653ebb4f
Leon <lowpolygonum@gmail.com>
2026-02-19 01:07:41 +0100

    wip page layout

--- code/python/cms.py
+++ code/python/cms.py
@@ -39,6 +39,8 @@ def write_cell(page, data):
     page.write('\t</a>\n')
 
 def make():
+
+    # index with all projects
     index = open('index.html', 'w')
     index = open('index.html', 'a')
     header = open('code/template/header.html').read()
@@ -47,25 +49,29 @@ def make():
     index.write(header)
     index.write('<html>\n<body class="grid">\n')
 
-    files_paths = get_files_path('content/', 'md')
+    # find projects metadata
+    files_paths = get_files_path('content/', 'yml')
+    # files_paths = get_files_path('content/xr/dusted', 'yml')
     pages_data = []
     for file_path in files_paths:
         file = open(file_path)
-        header = get_yaml(file)
-        body = file.read().replace(header, "")
-        yml = yaml.safe_load(header)
+        # header = get_yaml(file)
+        # body = file.read().replace(header, "")
+        # yml = yaml.safe_load(header)
+        yml = yaml.safe_load(file)
         array = file_path.split('/')
         slug = array.pop().replace('.md','')
         path = '/'.join(array)
         data = {
             'file_path': file_path,
-            'body': body,
+            # 'body': body,
             'yml': yml,
             'slug': slug,
             'path': path,
         }
         pages_data.append(data)
 
+    # sort projects
     def sort_page(item):
         number = 9999
         data = item['yml']
@@ -77,8 +83,11 @@ def make():
             number = data['order']
         return number
     pages_data = sorted(pages_data, key=sort_page, reverse=True)
-    # print(pages_data)
-    # return
+
+    link = yaml.safe_load(open('code/data/link.yml'))
+    
+    
+    # make pages for each projects
     for data in pages_data:
         path = data['path']
         page = open(path+'/index.html', 'w')
@@ -94,15 +103,74 @@ def make():
         page.write('<head>\n')
         page.write('<body>\n')
 
-        page.write(open('code/template/background.html').read())
+        # page.write(open('code/template/background.html').read())
         page.write(open('code/template/home_button.html').read())
 
         # page.write(body)
-        body = data['body']
-        lines = body.split('\n')
-        for line in lines:
-            page.write('\t'+line+'\n')
+        # body = data['body']
+        # lines = body.split('\n')
+        # for line in lines:
+        #     page.write('\t'+line+'\n')
+        yml = data['yml']
+        title = yml['title']
+        tagline = yml['tagline']
+        category = yml['category']
+        date = str(yml['date'])
+        info = None
+        if 'info' in yml:
+            info = yml['info']
+        media = yml['media']
 
+        authors = None
+        if 'with' in yml:
+            authors = yml['with']
+
+        page.write('\t<div class="page">\n')
+        page.write('\t\t<div class="info">\n')
+
+        # info
+        page.write('\t\t\t<h1>'+title+'</h1>\n')
+        page.write('\t\t\t<h2>'+tagline+'</h2>\n')#+' ('+date+')</h2>\n')
+        if info:
+            for line in info:
+                page.write('\t\t\t<p>'+line+'</p>\n')
+
+        # authors
+        if authors:
+            page.write('\t\t\t<p>')
+            page.write('<a href="https://neondelice.xyz/">Léon&nbsp;Denise</a>')
+            for line in authors:
+                name = line.replace(' ', '&nbsp;')
+                if line in link:
+                    page.write(', <a href="'+link[line]+'">'+name+'</a>')
+                else:
+                    page.write(', '+name)
+            page.write('\n\t\t\t</p>\n')
+
+        # category
+        page.write('\t\t\t<p><span class="tag">project</span>')
+        for cat in category:
+            page.write(', <span class="tag">'+cat+'</span>')
+        page.write('</p>\n')
+
+        # date
+        page.write('\t\t<p>'+date+'</p>\n')
+        page.write('\t\t</div>\n')
+
+        # media
+        page.write('\t\t<div class="media">\n')
+        for line in media:
+            if isinstance(line, list):
+                page.write('\t\t\t<div class="media-band">\n')
+                for l in line:
+                    page.write('\t\t\t\t<img src="media/'+l+'" />\n')
+                page.write('\t\t\t</div>\n')
+            elif "mp4" in line:
+                page.write('\t\t\t<video src="media/'+line+'" controls poster="media/poster.webp"></video>\n')
+            else:
+                page.write('\t\t\t<img src="media/'+line+'" />\n')
+        page.write('\t\t</div>\n')
+        page.write('\t</div>\n')
         page.write('</body>\n')
         page.write('</html>\n')
 
--- code/python/watch.py
+++ code/python/watch.py
@@ -1,27 +1,44 @@
 import sys, time, os
 import cms
 
-files = cms.get_files_path('content/', 'md')
+files = cms.get_files_path('content/', 'yml')
 cache = {}
 for file in files:
-    cache[file] = 0
+    cache[file] = os.stat(file).st_mtime
 
 while True:
-    try:
-        time.sleep(1)
-        files = cms.get_files_path('content/', 'md')
-        for file in files:
-            stamp = os.stat(file).st_mtime
-            if file in files == False:
-                cache[file] = 0
-            if stamp != cache[file]:
-                cache[file] = stamp
-                cms.make()
-                print('updated: '+file+' at ' + str(stamp) + ' \n')
+    # try:
+    #     time.sleep(1)
+    #     files = cms.get_files_path('content/', 'yml')
+    #     changed = False;
+    #     for file in files:
+    #         stamp = os.stat(file).st_mtime
+    #         if file in cache == False or stamp != cache[file]:
+    #             cache[file] = stamp
+    #             changed = True
+    #             # print('updated: '+file+' at ' + str(stamp) + ' \n')
+    #     if changed:    
+    #         cms.make()
 
-    except KeyboardInterrupt:
-        print('\nDone')
-        break
-    except:
-        print(f'Unhandled error: { sys.exc_info()[0] }')
-        break
+    # except KeyboardInterrupt:
+    #     print('\nDone')
+    #     break
+    # except:
+    #     print(f'Unhandled error: { sys.exc_info()[0] }')
+    #     break
+
+    time.sleep(1)
+    new_files = cms.get_files_path('content/', 'yml')
+    if len(new_files) != len(files):
+        for file in files:
+            cache[file] = os.stat(file).st_mtime
+        cms.make()
+    files = new_files
+    for file in files:
+        stamp = os.stat(file).st_mtime
+        if stamp != cache[file]:
+            cache[file] = stamp
+            cms.make()
+            
+            # print('updated: '+file+' at ' + str(stamp) + ' \n')
+        
